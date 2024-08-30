881358a298f98a4f72c884b4411bca591bd5be45
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2024-08-30 18:38:31 +0100

    create basic javascript file and added comments and concatenate two string and use expresions.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Script/Hello_world.js
@@ -0,0 +1,5 @@
+
+var text1 = "There is a String.", conca = " Which is now Concatenated!", animal = "Dog", value1=25, value2=30, value3=5; //assign multiple variable with multiple values
+window.alert(text1);    //printing one of the variable in alert.
+document.write(text1 + conca+" &nbsp;&nbsp;&nbsp;"); //concatenate two strings and added extra spaces 
+document.write(value1 + value2 * value3); // this is the sum of first two value multiplyed with third value
