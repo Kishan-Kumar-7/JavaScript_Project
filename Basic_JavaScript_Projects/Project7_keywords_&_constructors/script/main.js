8b961e5d03333348de9cbda736c4a75ca92bae79
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2024-09-05 14:37:22 +0100

    Project7 create and added nested function

--- Basic_JavaScript_Projects/Project7_keywords_&_constructors/script/main.js
+++ Basic_JavaScript_Projects/Project7_keywords_&_constructors/script/main.js
@@ -1,14 +1,35 @@
-function vehical(make, model, color, year) { 
-    this.make = make;
-    this.model = model;
-    this.color = color;
-    this.year = year;
+function Vehical(make, model, color, year) {
+    this.Make = make;
+    this.Model = model;
+    this.Color = color;
+    this.Year = year;
+}
+ 
+var Jack = new Vehical("Dodge", "Viper", "Black", 2020);
+var Emily = new Vehical("Jeep", "Trail Hawk", "Red and White", 2019);
+var Erik = new Vehical("Ford", "Pinto", "Musterd", 1971);
+
+function myCons(Name) { 
+    document.getElementById('vehi').innerHTML = Name+" Drives a " + Name.Make +" "+ Name.Model + " Coloured " + Name.Color + " Manufactured in Year " + Name.Year + ".";
 }
 
-var Jack = new vehical("Dodge", "Viper", "Black",2020);
-var Emily = new vehical("Jeep", "Trail Hawk", "White and Red",2019);
-var Erik = new vehical("Ford", "Pinto", "Mustard", 1971);
+console.log(Emily);
+console.log(Erik);
 
-function myCons(name) { 
-    document.getElementById('vehi').innerHTML = name + " Drives a " + name.vehical.make + " Model " + name.vehical.model + " Colored " + name.vehical.color + " Manufactured in Year " + name.v.year + ".";
+function count() { 
+    document.getElementById('counting').innerHTML = countF();
+    function countF() { 
+        var starting = 9;
+        function plus_one() { 
+            starting += 1;
+        }
+        plus_one();
+        return starting;
+    } 
 }
+
+function Count() {
+    var i = 0;
+    i++;
+    document.getElementById('result').innerHTML = i
+ }
