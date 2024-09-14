45e7d1a2d649a7f919253b9f2160feab04e359a3
Kishan-Kumar-7 <pkishankumar4@gmail.com>
2024-09-14 13:06:50 +0100

    update changes in file of extra.js to main.js with commets

--- Project_Calculator/script/main.js
+++ Project_Calculator/script/main.js
@@ -14,20 +14,19 @@ const Calculator = {
 function Input_Digit(digit) {
   const { Display_Value, Wait_Second_Operand } = Calculator;
   //This checks if the Wait_Second_Operand is true and sets Display_Value to the key that was clicked on.
-  if (Calculator.Wait_Second_Operand === true) {
+  if (Wait_Second_Operand === true) {
     Calculator.Display_Value = digit;
     Calculator.Wait_Second_Operand = false;
   } else {
     //This overwrites Display_Value if the current value is 0 otherwise it adds onto it.
     Calculator.Display_Value =
-      Calculator.Display_Value === "0"
-        ? digit
-        : Calculator.Display_Value + digit;
+      Display_Value === "0"
+        ? digit : Display_Value + digit;
   }
 }
 
 //this section handles decimal points.
-function Input_Digit(dot) {
+function Input_Decimal(dot) {
   // This ensures that accidental clicking of the decimal point doesn't cause bugs in the operation.
   if (Calculator.Wait_Second_Operand === true) return;
   if (!Calculator.Display_Value.includes(dot)) {
@@ -41,22 +40,18 @@ function Handles_Operator(Next_Operator) {
   const { First_Operand, Display_Value, operator } = Calculator;
   //When an operator key is pressed we convert the current number displayed on the screento a number and than store the result in Calculator.First_Operand if it doesn't already exist.
   const Value_of_Input = parseFloat(Display_Value);
-  console.log(typeof Value_of_Input);
   //Checks if an operator already exists and if Wait_Second_Operand is true, then updates the operator an exits from the function.
-  if (Calculator.operator && Calculator.Wait_Second_Operand) {
+  if (operator && Calculator.Wait_Second_Operand) {
     Calculator.operator = Next_Operator;
     return;
   }
   if (First_Operand == null) {
-    Calculator.First_Operand = Calculator.Value_of_Input;
-  } else if (Calculator.operator) {
+    Calculator.First_Operand = Value_of_Input;
+  } else if (operator) {
     //Checks if an operator already exists
-    const Value_Now = Calculator.First_Operand || 0;
+    const Value_Now = First_Operand || 0;
     //If operator exists, property lookup if performed for the operator in the Perform_Calculation object and function that matches the operator is executed.
-    let result = Perform_Calculation[operator](
-      Calculator.Value_Now,
-      Calculator.Value_of_Input
-    );
+    let result = Perform_Calculation[operator](Value_Now,Value_of_Input);
     //Here we add a fixed amount of numbers after the decimal.
     result = Number(result).toFixed(9);
     // This will remove any trailing 0's
@@ -65,7 +60,7 @@ function Handles_Operator(Next_Operator) {
     Calculator.First_Operand = parseFloat(result);
   }
   Calculator.Wait_Second_Operand = true;
-  Calculator.operator = Calculator.Next_Operator;
+  Calculator.operator = Next_Operator;
 }
 
 const Perform_Calculation = {
@@ -73,7 +68,7 @@ const Perform_Calculation = {
   "*": (First_Operand, Second_Operand) => First_Operand * Second_Operand,
   "+": (First_Operand, Second_Operand) => First_Operand + Second_Operand,
   "-": (First_Operand, Second_Operand) => First_Operand - Second_Operand,
-  "=": (First_Operand, Second_Operand) => Second_Operand,
+  "=": (Second_Operand) => Second_Operand
 };
 
 function Calculator_Reset() {
